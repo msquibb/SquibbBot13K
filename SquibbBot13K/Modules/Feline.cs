ecc273c2d57edc5e49047f0a04523229f048c2b1
Mike Squibb <msquibb@genesishcs.org>
2021-01-11 13:44:17 -0500

    Started pet module

---
+++ SquibbBot13K/Modules/Feline.cs
@@ -0,0 +1,18 @@
+﻿using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SquibbBot13K.Modules
+{
+    public class Feline : ModuleBase
+    {
+        public async Task HasBeenFed()
+        {
+            var PetName ="Tina";
+            await ReplyAsync($"{PetName} has been fed. Don't fall for her bullshit!");
+        }
+    }
+}
